744651b86ad0c740e9fac653826332812c779b54
Peter A. Kurishev <peter.kurishev@gmail.com>
2014-11-11 22:44:24 +0300

    config changes for Heroku

--- smyt_test/wsgi.py
+++ smyt_test/wsgi.py
@@ -1,5 +1,8 @@
 from django.core.wsgi import get_wsgi_application
+import os
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'smyt_test.settings')
+
 from dj_static import Cling
 
 application = Cling(get_wsgi_application())
